1693b30fe79bfbbf725906842bf6da455fa51e42
sg9031 <sg9031@gmail.com>
2021-02-06 00:17:58 +0400

    display badge on linkedin.com

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -3,3 +3,21 @@ import '../../assets/img/icon-128.png';
 
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
+
+chrome.tabs.onUpdated.addListener(
+	function (tabId, changeInfo, tab) {
+		// read changeInfo data and do something with it
+		// like send the new url to contentscripts.js
+		if (changeInfo.url && changeInfo.url.indexOf("linkedin.com") > -1) {
+			chrome.browserAction.setBadgeText({ text: "notes" });
+			chrome.tabs.sendMessage(tabId, {
+				message: 'hello!',
+				url: changeInfo.url
+			})
+		}
+		if (changeInfo.url && changeInfo.url.indexOf("linkedin.com") == -1) {
+			chrome.browserAction.setBadgeText({ text: null });
+		}
+	}
+);
+
