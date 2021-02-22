93e5bcf1efd8d53a6e36bf91a8719d1f3bb08380
Stanley SERBIN <stanleyserbin@gmail.com>
2021-02-22 16:24:24 +0100

    UPDATE api url

--- src/pages/api.jsx
+++ src/pages/api.jsx
@@ -1,6 +1,7 @@
 // const url = 'http://51.77.245.132:5000';
 // const url = 'http://localhost:5000';
-const url = 'https://networkflow-dev.herokuapp.com';
+// const url = 'https://networkflow-dev.herokuapp.com';
+const url = 'https://networkflow-prod.herokuapp.com';
 // const url = 'http://localhost:8000';
 
 // const meili_url = 'http://localhost:7700';
